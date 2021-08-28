7760ee366d1eaa56b13518af394ee5a2c527bd02
Insopitus <jessefor@hotmail.com>
2021-08-29 00:22:01 +0800

    add build folder; performance update (reduce gc)

--- src/Graph.ts
+++ src/Graph.ts
@@ -1,6 +1,6 @@
 const SIN60 = Math.sin(Math.PI / 3)
 const COS60 = Math.cos(Math.PI / 3)
-let x=0,y=0 //reduce gc
+let x = 0, y = 0 //reduce gc
 export default class Graph {
   #context: CanvasRenderingContext2D
   constructor(query: string, options: GraphOptions) {
@@ -39,26 +39,26 @@ export default class Graph {
     ctx.strokeStyle = 'black'
     // u axis
     for (let i = 1; i <= 5; i++) {
-      const pos = getCanvas2DCoord(.2 * i, 0, 1-.2*1)
-      applyTranformation(pos,triangleEdgeLength,[offsetX,offsetY])
-      ctx.moveTo(pos[0] , pos[1] )
-      ctx.lineTo(pos[0]  - scaleSize * COS60, pos[1]  + scaleSize * SIN60)
+      const pos = getCanvas2DCoord(.2 * i, 0, 1 - .2 * 1)
+      applyTranformation(pos, triangleEdgeLength, [offsetX, offsetY])
+      ctx.moveTo(pos[0], pos[1])
+      ctx.lineTo(pos[0] - scaleSize * COS60, pos[1] + scaleSize * SIN60)
       ctx.stroke()
     }
     // v axis
     for (let i = 1; i <= 5; i++) {
       const pos = getCanvas2DCoord(1 - .2 * i, 0.2 * i, 0)
-      applyTranformation(pos,triangleEdgeLength,[offsetX,offsetY])
+      applyTranformation(pos, triangleEdgeLength, [offsetX, offsetY])
       ctx.moveTo(pos[0], pos[1])
-      ctx.lineTo(pos[0]  + scaleSize, pos[1] )
+      ctx.lineTo(pos[0] + scaleSize, pos[1])
       ctx.stroke()
     }
     // w axis
     for (let i = 1; i <= 5; i++) {
       const pos = getCanvas2DCoord(0, 1 - .2 * i, .2 * i)
-      applyTranformation(pos,triangleEdgeLength,[offsetX,offsetY])
-      ctx.moveTo(pos[0], pos[1] )
-      ctx.lineTo(pos[0]  - scaleSize * COS60, pos[1]  - scaleSize * SIN60)
+      applyTranformation(pos, triangleEdgeLength, [offsetX, offsetY])
+      ctx.moveTo(pos[0], pos[1])
+      ctx.lineTo(pos[0] - scaleSize * COS60, pos[1] - scaleSize * SIN60)
       ctx.stroke()
     }
 
@@ -68,16 +68,22 @@ export default class Graph {
   }
   private drawPoints(content: GraphOptions['data'], triangleEdgeLength: number, offsetX: number, offsetY: number) {
     if (!content || content.length === 0) return
+    let x = 0  //reduce gc
+    let y = 0
     const ctx = this.#context
     ctx.strokeStyle = 'dodgerblue'
     for (let point of content) {
-      const pos = getCanvas2DCoord(...point.coordinate)
-      applyTranformation(pos,triangleEdgeLength,[offsetX,offsetY])      
+      [x, y] = getCanvas2DCoord(...point.coordinate)
+      // avoiding using `applyTransformation` to reduce gc
+      x *= triangleEdgeLength
+      x += offsetX
+      y *= triangleEdgeLength
+      y += offsetY
       if (point.type === 'dot') {
         if (point.dotColor) ctx.fillStyle = point.dotColor
         const dotSize = point.dotSize || 5
         ctx.beginPath()
-        ctx.arc(pos[0],pos[1],dotSize,0,Math.PI*2)
+        ctx.arc(x, y, dotSize, 0, Math.PI * 2)
         ctx.fill()
       }
     }
@@ -126,10 +132,10 @@ export function getCanvas2DCoord(u: number, v: number, w: number): [number, numb
   return [x, y]
 }
 
-function applyTranformation(position:[number,number],scale:number,translate:[number,number]){
-  position[0]*=scale
-  position[1]*=scale
-  position[0]+=translate[0]
-  position[1]+=translate[1]
+function applyTranformation(position: [number, number], scale: number, translate: [number, number]) {
+  position[0] *= scale
+  position[1] *= scale
+  position[0] += translate[0]
+  position[1] += translate[1]
   return position
 }
