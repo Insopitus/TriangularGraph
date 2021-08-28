b358e0c989e6aac91b6603079f673fc93877f55d
Insopitus <jessefor@hotmail.com>
2021-08-29 03:37:04 +0800

    add axis titles

--- script.ts
+++ script.ts
@@ -1,37 +1,76 @@
 
 import Graph from "./src/Graph";
-const POINT_COUNT = 1e5
-const data = new Array(POINT_COUNT)
-for(let i=0;i<POINT_COUNT;i++){
-  const u = Math.random()
-  const v = Math.min(Math.random(),1-u)
-  const w=0
-  data[i]={
-    type:'dot',
-    dotColor:'red',
-    dotSize:2,
-    coordinate:[u,v,w]
-  }
-}
-const graph = new Graph('.graph',{
-  width:1200,
-  height:800,
-  title:[
+
+
+const option = {
+  width: 1200,
+  height: 800,
+  title: [
     {
-      text:'U Axis',
-      fontSize:20
+      text: 'U Axis',
+      fontSize: 20
     },
     {
-      text:'V Axis',
-      fontSize:20
+      text: 'V Axis',
+      fontSize: 20
     },
     {
-      text:'W Axis',
-      fontSize:20
+      text: 'W Axis',
+      fontSize: 20
     },
   ],
-  data
-})
+  data:generatePoints()
+}
+const graph = new Graph('.graph', option)
+// animate()
+function animate(){
+  const option = {
+    width: 1200,
+    height: 800,
+    title: [
+      {
+        text: 'U Axis',
+        fontSize: 20
+      },
+      {
+        text: 'V Axis',
+        fontSize: 20
+      },
+      {
+        text: 'W Axis',
+        fontSize: 20
+      },
+    ],
+    data:generatePoints()
+  }
+  
+  graph.render(option.data)
+  requestAnimationFrame(animate)
+}
+function generatePoints() {
+  const POINT_COUNT = 1e1
+  const data = new Array(POINT_COUNT)
+  for (let i = 0; i < POINT_COUNT; i++) {
+    let u = Math.random()
+    let v = Math.random()
+    let w = Math.random()
+    const sum = u + v + w
+    u = u / sum
+    v = v / sum
+    w = w / sum
+    const colorU = Math.trunc(u * 0xff).toString(16).padStart(2, '0')
+    const colorV = Math.trunc(v * 0xff).toString(16).padStart(2, '0')
+    const colorW = Math.trunc(w * 0xff).toString(16).padStart(2, '0')
+    data[i] = {
+      type: 'dot',
+      dotColor: '#' + colorU + colorV + colorW,
+      dotSize: 5,
+      coordinate: [u, v, w]
+    }
+    // debugger
+  }
+  return data
+}
 
 
 
--- src/Graph.ts
+++ src/Graph.ts
@@ -1,29 +1,42 @@
 const SIN60 = Math.sin(Math.PI / 3)
 const COS60 = Math.cos(Math.PI / 3)
-let x = 0, y = 0 //reduce gc
 export default class Graph {
-  #context: CanvasRenderingContext2D
+  context: CanvasRenderingContext2D
+  options: GraphOptions
   constructor(query: string, options: GraphOptions) {
-    const width = options.width ?? 600
-    const height = options.height ?? 400
+    if (!options) throw 'option is needed.'
+    this.options = options
+    const width = options.width || 600
+    const height = options.height || 400
     const canvas = document.createElement('canvas')
     canvas.width = width
     canvas.height = height
     document.querySelector(query).appendChild(canvas)
-    this.#context = canvas.getContext('2d')
-    const offsetX = 100
-    const offsetY = 0
+    this.context = canvas.getContext('2d', { alpha: false })
+    this.context.fillStyle = 'gray'
+    this.context.fillRect(0, 0, width, height)
+    this.render(options.data)
+    // this.#context.translate(.5,.5)
+  }
+  render(data?: GraphOptions['data']) {
+    data ||= this.options.data
+    const offsetX = 200
+    const offsetY = 100
     const edgeLength = 600
-    console.time('render')
+    const t1 = performance.now()
     this.drawTriangle(edgeLength, offsetX, offsetY)
-    this.drawScale(options.scale, edgeLength, offsetX, offsetY)
-    this.drawPoints(options.data, edgeLength, offsetX, offsetY)
-    console.timeEnd('render')
+    this.drawScale(this.options.scale, edgeLength, offsetX, offsetY)
+    this.drawPoints(data, edgeLength, offsetX, offsetY)
+    this.drawTitle(this.options.title, edgeLength, offsetX, offsetY)
+    console.log(`Rendering ${data.length} points took ${performance.now() - t1} ms.`)
   }
   private drawTriangle(edgeLength: number, offsetX: number, offsetY: number) {
-    const ctx = this.#context
+    const ctx = this.context
     ctx.fillStyle = 'white'
     const triangleHeight = edgeLength * SIN60
+    ctx.beginPath()
+    ctx.strokeStyle = 'black'
+    ctx.lineWidth = 1
     ctx.moveTo(0 + offsetX, triangleHeight + offsetY)
     ctx.lineTo(.5 * edgeLength + offsetX, 0 + offsetY)
     ctx.lineTo(edgeLength + offsetX, triangleHeight + offsetY)
@@ -35,12 +48,13 @@ export default class Graph {
     option ||= {}
     const scaleSize = 10 // 
     if (option.disable) return
-    const ctx = this.#context
+    const ctx = this.context
     ctx.strokeStyle = 'black'
     // u axis
     for (let i = 1; i <= 5; i++) {
       const pos = getCanvas2DCoord(.2 * i, 0, 1 - .2 * 1)
       applyTranformation(pos, triangleEdgeLength, [offsetX, offsetY])
+      ctx.beginPath()
       ctx.moveTo(pos[0], pos[1])
       ctx.lineTo(pos[0] - scaleSize * COS60, pos[1] + scaleSize * SIN60)
       ctx.stroke()
@@ -49,6 +63,7 @@ export default class Graph {
     for (let i = 1; i <= 5; i++) {
       const pos = getCanvas2DCoord(1 - .2 * i, 0.2 * i, 0)
       applyTranformation(pos, triangleEdgeLength, [offsetX, offsetY])
+      ctx.beginPath()
       ctx.moveTo(pos[0], pos[1])
       ctx.lineTo(pos[0] + scaleSize, pos[1])
       ctx.stroke()
@@ -57,20 +72,57 @@ export default class Graph {
     for (let i = 1; i <= 5; i++) {
       const pos = getCanvas2DCoord(0, 1 - .2 * i, .2 * i)
       applyTranformation(pos, triangleEdgeLength, [offsetX, offsetY])
+      ctx.beginPath()
       ctx.moveTo(pos[0], pos[1])
       ctx.lineTo(pos[0] - scaleSize * COS60, pos[1] - scaleSize * SIN60)
       ctx.stroke()
     }
 
   }
-  private drawTitle(option: GraphOptions['title'], offsetX: number, offsetY: number) {
+  private drawTitle(data: GraphOptions['title'], edgeLength: number, offsetX: number, offsetY: number) {
+    if (!data) return
+    const margin = 50
+    const ctx = this.context
+    ctx.font = '30px Arial'
+    ctx.fillStyle = 'black'
+    ctx.textAlign = 'center'
 
+    if (!data[0].disable) {
+      // u axis
+      const pos1 = getCanvas2DCoord(.5, 0, .5)
+      applyTranformation(pos1, edgeLength, [offsetX, offsetY + margin])
+      ctx.beginPath()
+      ctx.textBaseline = 'top'
+      ctx.fillText(data[0].text, ...pos1)
+    }
+    if (!data[1].disable) {
+      // v axis
+      const pos2 = getCanvas2DCoord(.5, .5, 0)
+      applyTranformation(pos2, edgeLength, [offsetX + margin, offsetY])
+      // ctx.save()
+      ctx.beginPath()
+      // ctx.rotate(Math.PI/3)
+      ctx.textAlign = 'left'
+      ctx.textBaseline = 'middle'
+      ctx.fillText(data[1].text, ...pos2)
+    }
+    if (!data[2].disable) {
+      // w axis
+      const pos3 = getCanvas2DCoord(0, .5, 0.5)
+      applyTranformation(pos3, edgeLength, [offsetX - margin, offsetY])
+      // ctx.save()
+      ctx.beginPath()
+      // ctx.rotate(Math.PI/3)
+      ctx.textAlign = 'right'
+      ctx.textBaseline = 'middle'
+      ctx.fillText(data[2].text, ...pos3)
+    }
   }
   private drawPoints(content: GraphOptions['data'], triangleEdgeLength: number, offsetX: number, offsetY: number) {
     if (!content || content.length === 0) return
     let x = 0  //reduce gc
     let y = 0
-    const ctx = this.#context
+    const ctx = this.context
     ctx.strokeStyle = 'dodgerblue'
     for (let point of content) {
       [x, y] = getCanvas2DCoord(...point.coordinate)
